a421135cad555f12c89ea3bb341a6a5738db1e68
MarilynWaldman <mdwaldman22@hotmail.com>
2020-08-27 13:59:15 -0600

    load elasticsearch

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 #from elasticsearch import Elasticsearch, exceptions
-import os
-import time
-from flask import Flask, jsonify, request, render_template
-import sys
+#import os
+#import time
+#from flask import Flask, jsonify, request, render_template
+#import sys
 import requests
 import json
 
@@ -10,12 +10,17 @@ import json
 def load_data_in_es():
     """ creates an index in elasticsearch """
     url = "https://services3.arcgis.com/66aUo8zsujfVXRIT/arcgis/rest/services/Community_Testing_Sites/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
-
+    #url = "http://data.sfgov.org/resource/rqzj-sfat.json"
     r = requests.get(url)
     data = r.json()
     print("Loading data in elasticsearch ...")
+    #print(json.dumps(data, indent=2))
     #ßprint(data)
-    print(json.dumps(data, indent=2))
+    #print(json.dumps(data['features'], indent=2))
+    for id, site in enumerate(data['features']):
+      res = es.index(index="codata", doc_type="site", id=id, body=site)
+    print("Total sites loaded: ", len(data['features']))
+
 
 
 load_data_in_es()
